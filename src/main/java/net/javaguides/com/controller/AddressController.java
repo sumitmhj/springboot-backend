a9a70aab862a5246965ee052e6d39ca92f222988
Sumit Maharjan <sumitjan2@gmail.com>
2024-12-22 19:04:34 +1100

    Address class has @GetMapping and @PostMapping working

--- src/main/java/net/javaguides/com/controller/AddressController.java
+++ src/main/java/net/javaguides/com/controller/AddressController.java
@@ -1,2 +1,27 @@
-package net.javaguides.com.controller;public class AddressController {
+package net.javaguides.com.controller;
+
+import net.javaguides.com.model.Address;
+import net.javaguides.com.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/v1/addresses")
+public class AddressController {
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @GetMapping
+    public List<Address> getAllAddresses(){
+        return addressRepository.findAll();
+    }
+
+    @PostMapping
+    public Address createAddress(@RequestBody Address address){
+        return addressRepository.save(address);
+    }
 }
--- src/main/java/net/javaguides/com/model/Address.java
+++ src/main/java/net/javaguides/com/model/Address.java
@@ -22,11 +22,43 @@ public class Address implements Serializable {
     private String postCode;
     private String country;
 
-    @OneToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name="emp_id", nullable = false)
-    private Employee employee;
+    public String getStreet() {
+        return street;
+    }
 
+    public void setStreet(String street) {
+        this.street = street;
+    }
 
+    public String getCity() {
+        return city;
+    }
 
+    public void setCity(String city) {
+        this.city = city;
+    }
 
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
